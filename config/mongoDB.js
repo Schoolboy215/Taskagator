2865b1dc24901ac94fee4879e17594f0bc8dfcdf
James McKay <james.mckay215@yahoo.com>
2018-03-15 14:53:32 -0400

    This file accidentally got committed.
    Credentials will be changed after this

--- config/mongoDB.js
+++
@@ -1,1 +0,0 @@
-exports.connectionString = "mongodb://jmckay:fitz7000@cluster0-shard-00-00-xb5wm.mongodb.net:27017,cluster0-shard-00-01-xb5wm.mongodb.net:27017,cluster0-shard-00-02-xb5wm.mongodb.net:27017/taskagator?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
